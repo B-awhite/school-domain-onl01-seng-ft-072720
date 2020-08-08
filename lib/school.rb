d3a26670c98658569327cf436370293465c85d42
Bria White <bria.awhite@gmail.com>
2020-08-08 14:24:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,6 +7,13 @@ class School
     @roster = {}
   end   
   
-  
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
   
 end   
